8987e50ce5122ccd38013025a79922390c146033
TheBobBob <bhavyak7@uw.edu>
2026-04-02 02:40:31 -0700

    Updated default values of gradient_clip_val and gradient_clip_algorithm (#600)
    * Updated config file
    
    * Fix CHANGELOG.md
    
    * Fix Python target version for black
    
    * Addressed test discrepancy
    
    * Disabled changes
    
    * Added changes
    
    * Changed conftest
    
    * Updated test to accomodate new gradient clipping behavior
    
    * Addressed coderabbit suggestions
    
    * Increased max_epochs
    
    * Increased max_epochs
    
    * Updated config file step
    
    * Updated test failings
    
    * Updated tests
    
    ---------
    
    Co-authored-by: Bhavya Krishnan <bhavyak7@cysteine.gs.washington.edu>
    Co-authored-by: Wout Bittremieux <bittremieux@users.noreply.github.com>
    Co-authored-by: Bhavya Krishnan <bhavyak7@nexus1.gs.washington.edu>

--- tests/unit_tests/test_unit.py
+++ tests/unit_tests/test_unit.py
@@ -462,7 +462,10 @@ def test_setup_model(monkeypatch):
     version = "3.0.0"
 
     # Test model is none when not training
-    with monkeypatch.context() as mnk, tempfile.TemporaryDirectory() as tmp_dir:
+    with (
+        monkeypatch.context() as mnk,
+        tempfile.TemporaryDirectory() as tmp_dir,
+    ):
         mnk.setattr(casanovo, "__version__", version)
         mnk.setattr("appdirs.user_cache_dir", lambda n, a, opinion: tmp_dir)
         mnk.setattr(github, "Github", mock_github)
@@ -482,7 +485,10 @@ def test_setup_model(monkeypatch):
         assert not filename.is_file()
         assert mock_get.request_counter == 1
 
-    with monkeypatch.context() as mnk, tempfile.TemporaryDirectory() as tmp_dir:
+    with (
+        monkeypatch.context() as mnk,
+        tempfile.TemporaryDirectory() as tmp_dir,
+    ):
         mnk.setattr(casanovo, "__version__", version)
         mnk.setattr("appdirs.user_cache_dir", lambda n, a, opinion: tmp_dir)
         mnk.setattr(github, "Github", mock_github)
@@ -513,9 +519,11 @@ def test_setup_model(monkeypatch):
         assert mock_get.request_counter == 3
 
     # Test model is file
-    with monkeypatch.context() as mnk, tempfile.NamedTemporaryFile(
-        suffix=".ckpt"
-    ) as temp_file, tempfile.TemporaryDirectory() as tmp_dir:
+    with (
+        monkeypatch.context() as mnk,
+        tempfile.NamedTemporaryFile(suffix=".ckpt") as temp_file,
+        tempfile.TemporaryDirectory() as tmp_dir,
+    ):
         mnk.setattr(casanovo, "__version__", version)
         mnk.setattr("appdirs.user_cache_dir", lambda n, a, opinion: tmp_dir)
         mnk.setattr(github, "Github", mock_github)
@@ -535,7 +543,10 @@ def test_setup_model(monkeypatch):
         assert result == temp_file_path
 
     # Test model is neither a URL or File
-    with monkeypatch.context() as mnk, tempfile.TemporaryDirectory() as tmp_dir:
+    with (
+        monkeypatch.context() as mnk,
+        tempfile.TemporaryDirectory() as tmp_dir,
+    ):
         mnk.setattr(casanovo, "__version__", version)
         mnk.setattr("appdirs.user_cache_dir", lambda n, a, opinion: tmp_dir)
         mnk.setattr(github, "Github", mock_github)
@@ -564,7 +575,10 @@ def test_get_model_weights(monkeypatch):
     mock_github = functools.partial(MockGithub, test_releases)
 
     for version in test_releases:
-        with monkeypatch.context() as mnk, tempfile.TemporaryDirectory() as tmp_dir:
+        with (
+            monkeypatch.context() as mnk,
+            tempfile.TemporaryDirectory() as tmp_dir,
+        ):
             mnk.setattr(casanovo, "__version__", version)
             mnk.setattr(
                 "appdirs.user_cache_dir", lambda n, a, opinion: tmp_dir
@@ -584,7 +598,10 @@ def test_get_model_weights(monkeypatch):
     # Impossible to find model weights for (i) full version mismatch and (ii)
     # major version mismatch.
     for version in ["999.999.999", "999.0.0"]:
-        with monkeypatch.context() as mnk, tempfile.TemporaryDirectory() as tmp_dir:
+        with (
+            monkeypatch.context() as mnk,
+            tempfile.TemporaryDirectory() as tmp_dir,
+        ):
             mnk.setattr(casanovo, "__version__", version)
             mnk.setattr(github, "Github", mock_github)
             mnk.setattr(requests, "get", mock_get)
@@ -597,7 +614,10 @@ def test_get_model_weights(monkeypatch):
             403, "API rate limit exceeded", None
         )
 
-    with monkeypatch.context() as mnk, tempfile.TemporaryDirectory() as tmp_dir:
+    with (
+        monkeypatch.context() as mnk,
+        tempfile.TemporaryDirectory() as tmp_dir,
+    ):
         mnk.setattr("appdirs.user_cache_dir", lambda n, a, opinion: tmp_dir)
         mnk.setattr("github.Requester.Requester.requestJsonAndCheck", request)
         mnk.setattr(requests, "get", mock_get)
@@ -611,7 +631,10 @@ def test_get_model_weights(monkeypatch):
 def test_get_weights_from_url(monkeypatch):
     file_url = "http://example.com/model_weights.ckpt"
 
-    with monkeypatch.context() as mnk, tempfile.TemporaryDirectory() as tmp_dir:
+    with (
+        monkeypatch.context() as mnk,
+        tempfile.TemporaryDirectory() as tmp_dir,
+    ):
         mock_get = MockResponseGet()
         mock_head = MockResponseHead()
         mnk.setattr(requests, "get", mock_get)
